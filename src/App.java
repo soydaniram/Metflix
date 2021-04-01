2ca0d043e9d5106bf22759e4b9e69a4acce6ff9a
soydaniram <DaniAleRam18@Gmail.com>
2021-03-31 21:45:10 -0300

    Primer Proyecto Mextflix

---
+++ src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //A partir de Metflix, buscar la temporada 5 episodio 1 de la serie
+        //How I met your mother y 
+        //reproducir el episodio.
+
+        Metflix miMetflix = new Metflix();
+
+        System.out.println("Inicializando catalogo de Metflix");
+
+        miMetflix.inicializarCatalogo();//Estoy llamando al metodo que crea las pelis y series.
+
+        Serie serieBuscada = miMetflix.buscarSerie("How I met your mother");
+
+        Temporada temporada = serieBuscada.buscarTemporada(5);
+
+        Episodio episodio = temporada.buscarEpisodio(1);
+
+        episodio.reproducir();
+
+
+
+    }
+}
